50b46e2ad0fe4d0c863c9dfdf24341f98dfb21d9
Vadim <zzcrash@gmail.com>
2019-12-05 11:20:49 +0200

    Create CW_Who_has_the_most money.rb

---
+++ CW_Who_has_the_most money.rb
@@ -0,0 +1,30 @@
+# Solution for https://www.codewars.com/kata/who-has-the-most-money/train/ruby
+
+class Student
+  attr_reader :name
+  attr_reader :fives
+  attr_reader :tens
+  attr_reader :twenties
+
+  def initialize(name, fives, tens, twenties)
+    @name = name
+    @fives = fives
+    @tens = tens
+    @twenties = twenties
+  end
+end
+
+phil = Student.new("Phil", 2, 2, 1)
+cam = Student.new("Cameron", 2, 2, 0)
+geoff = Student.new("Geoff", 0, 3, 0)
+
+def most_money(students)
+  # NOTE: the Student class is preloaded
+  arr = []
+  students.map do |x|
+    arr << "#{x.name} #{x.fives * 5 + x.tens * 10 + x.twenties * 20}"
+  end
+  arr.sort.last.split(' ')[0]
+end
+
+most_money([cam, phil, geoff])
