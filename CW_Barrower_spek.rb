b6c82d123142f37743e333f655846d31a3c70a93
Vadim <zzcrash@gmail.com>
2019-12-12 13:35:39 +0200

    Refactoring #2

--- CW_Barrower_spek.rb
+++ CW_Barrower_spek.rb
@@ -1,6 +1,8 @@
 # Solution for https://www.codewars.com/kata/borrower-speak/train/ruby
 # I can use next solution but its easy (s.downcase.gsub(/[-!.?,:;_ \+]/,"")
 
+def borrow(s)
   res = []
   a = s.downcase.each_byte { |x| res << x.ord }
-  res.map! { |x| x.chr if x.to_i >= 96}.join
+  res.map! { |x| x.chr if x >= 96 || x >= 122 }.join
+end
