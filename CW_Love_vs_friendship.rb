749c5e7ac5580920252f247a544a18721e83a379
Vadim <zzcrash@gmail.com>
2019-12-12 12:04:44 +0200

    Refactoring

--- CW_Love_vs_friendship.rb
+++ CW_Love_vs_friendship.rb
@@ -1,8 +1,5 @@
 # Solution for https://www.codewars.com/kata/love-vs-friendship/train/ruby
 
 def words_to_marks(string)
-  res = []
-  splited_text = string.split('')
-  splited_text.map { |x| res << x.ord - 96}
-  res.sum
+  string.bytes.map { |x| x - 96}.sum
 end
