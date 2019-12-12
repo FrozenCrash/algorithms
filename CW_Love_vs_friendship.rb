f37f1d578549d23f7deb4f643e2f6b44c305bc10
Vadim <zzcrash@gmail.com>
2019-12-12 11:38:57 +0200

    New code
    Refactoring all code, using ord.

--- CW_Love_vs_friendship.rb
+++ CW_Love_vs_friendship.rb
@@ -1,11 +1,8 @@
 # Solution for https://www.codewars.com/kata/love-vs-friendship/train/ruby
 
 def words_to_marks(string)
-  a = ("a".."z").to_a
   res = []
   splited_text = string.split('')
-  a.each_with_index do |val, ind|
-    splited_text.map { |st| res << ind + 1 if st == val }
-  end
+  splited_text.map { |x| res << x.ord - 96}
   res.sum
 end
