a9b5ae96cd4fc0f0d57d86087478501fbc6af70c
Vadim <zzcrash@gmail.com>
2019-12-27 15:40:17 +0200

    Refactoring
    Less code refactoring

--- CW_Santa's_Naughty_List.rb
+++ CW_Santa's_Naughty_List.rb
@@ -1,6 +1,5 @@
 # Solution for https://www.codewars.com/kata/santas-naughty-list/train/ruby
 
 def find_children(santas_list, children)
-  a = santas_list - children
-  (santas_list - a).sort
+  santas_list - (santas_list - children)
 end
