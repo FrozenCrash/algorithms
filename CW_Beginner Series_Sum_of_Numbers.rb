7a58f199f6b74b2ceecb2504ca1bb3a52fc8951b
Vadim <zzcrash@gmail.com>
2019-12-28 09:50:32 +0200

    Refactoring
    Refactoring if/else statement

--- CW_Beginner Series_Sum_of_Numbers.rb
+++ CW_Beginner Series_Sum_of_Numbers.rb
@@ -1,9 +1,5 @@
 # Solution for https://www.codewars.com/kata/beginner-series-number-3-sum-of-numbers/train/ruby
 
 def get_sum(a,b)
-  if a < b
-    (a..b).sum
-   else
-    (b..a).sum
-  end
+  a < b ? (a..b).sum : (b..a).sum
 end
