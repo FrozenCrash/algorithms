841c44b3b8a5242960592cd4031213ce4f27ca22
Vadim <zzcrash@gmail.com>
2021-01-04 16:41:21 +0200

    Create CW_Switch_it_up.rb

---
+++ CW_Switch_it_up.rb
@@ -0,0 +1,5 @@
+#Solution for https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/
+def switch_it_up(number)
+  numb_str = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+  numb_str[number]  
+end
