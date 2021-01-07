092a78d8a7f3713687ee80f5ca9e585e9b2649da
Vadim <zzcrash@gmail.com>
2021-01-07 19:39:19 +0200

    Update CW_Hello_name_or_world.rb

--- CW_Hello_name_or_world.rb
+++ CW_Hello_name_or_world.rb
@@ -1,5 +1,6 @@
 #Solution for https://www.codewars.com/kata/57e3f79c9cb119374600046b/
 def hello(*name)
   a = "Hello, World!"
-  name[0] != nil ? ( name[0].length > 0 ? "Hello, #{name[0].capitalize}!" : a ) : a
+  n = name[0]
+  n != nil ? ( n.length > 0 ? "Hello, #{n.capitalize}!" : a ) : a
 end
