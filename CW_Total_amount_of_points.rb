23c0ae96d5256c0f02147adefb4d8f4b9a07f6b8
Vadim <zzcrash@gmail.com>
2021-01-11 21:06:21 +0200

    Update CW_Total_amount_of_points.rb

--- CW_Total_amount_of_points.rb
+++ CW_Total_amount_of_points.rb
@@ -1,9 +1,4 @@
 #Solution for https://www.codewars.com/kata/5bb904724c47249b10000131/
 def points(games)
-  tp = 0
-  games.map do |g| 
-    sg = g.split(":")
-    sg[0].to_i > sg[1].to_i ? tp += 3 : sg[0].to_i == sg[1].to_i ? tp += 1 : 0
-  end
-  return tp
+  games.map { |g| g[0].to_i > g[2].to_i ? 3 : g[0].to_i == g[2].to_i ? 1 : 0 }.sum
 end
