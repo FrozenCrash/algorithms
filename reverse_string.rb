1a77476b6883e7406e35a5fdfc9d1b508ac69185
Vadim <zzcrash@gmail.com>
2021-03-09 15:23:43 +0200

    3 methods of reversing string

---
+++ reverse_string.rb
@@ -0,0 +1,28 @@
+# First method 
+
+def reverse_string(string)
+  splited_string = string.split('') # Split string
+  counter = string.length - 1       # Count elements in array
+  answ = Array.new                  # Create new array for answear
+  while counter >= 0 do             # Start loop
+    answ << splited_string[counter] # Insert last object form splited_string into answear array
+    counter -= 1                    
+  end
+  answ.join('')                     # Join our array
+end
+
+puts reverse_string('Hello World !')
+
+# Second method
+
+def rev_str(string)
+  b = Array.new                         # Create new array for result
+  string.chars.map { |x| b.unshift(x) } # Reverse our string
+  b.join 
+end
+
+puts rev_str("Hello Earth !")
+
+# Third method
+
+"string".reverse # :)
